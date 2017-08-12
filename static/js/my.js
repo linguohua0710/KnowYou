774832a6f7d087427d22c574ce74bd938319ec78
LinGH <lgh_1252518593@163.com>
2017-08-12 17:52:36 +0800

    delete overflow hidden

--- static/js/my.js
+++ static/js/my.js
@@ -46,23 +46,23 @@ $(document).ready(function(){
     /**
      * 页面随滚
      */
-    $(function() {
-    var $sidebar   = $("aside"),
-        $window    = $(window),
-        offset     = $sidebar.offset(),
-        topPadding = 15;
-
-    $window.scroll(function() {
-        if ($window.scrollTop() > offset.top) {
-            $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
-            });
-        } else {
-            $sidebar.stop().animate({
-                marginTop: 0
-            });
-        }
-    });
+    // $(function() {
+    // var $sidebar   = $("aside"),
+    //     $window    = $(window),
+    //     offset     = $sidebar.offset(),
+    //     topPadding = 15;
+    //
+    // $window.scroll(function() {
+    //     if ($window.scrollTop() > offset.top) {
+    //         $sidebar.stop().animate({
+    //             marginTop: $window.scrollTop() - offset.top + topPadding
+    //         });
+    //     } else {
+    //         $sidebar.stop().animate({
+    //             marginTop: 0
+    //         });
+    //     }
+    // });
 
 });
 
